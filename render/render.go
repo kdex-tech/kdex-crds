45f2c50f771523400701e3984e9bf7c5e201d5b5
Raymond Augé <rayauge@doublebite.com>
2025-10-25 11:13:52 -0400

    render more
    Signed-off-by: Raymond Augé <rayauge@doublebite.com>

--- render/render.go
+++ render/render.go
@@ -19,42 +19,13 @@ func (r *Renderer) RenderPage(page Page) (string, error) {
 	}
 
 	templateData := TemplateData{
-		FootScript:   template.HTML(r.FootScript),
-		HeadScript:   template.HTML(r.HeadScript),
 		Language:     r.Language,
 		Languages:    r.Languages,
 		LastModified: date,
-		PageMap:      pageMap,
-		Meta:         template.HTML(r.Meta),
 		Organization: r.Organization,
-		Stylesheet:   template.HTML(r.Stylesheet),
+		PageMap:      pageMap,
 		Title:        page.Title,
 	}
-
-	headerOutput, err := r.RenderOne(fmt.Sprintf("%s-header", page.TemplateName), page.Header, templateData)
-	if err != nil {
-		return "", err
-	}
-
-	templateData.Header = template.HTML(headerOutput)
-
-	footerOutput, err := r.RenderOne(fmt.Sprintf("%s-footer", page.TemplateName), page.Footer, templateData)
-	if err != nil {
-		return "", err
-	}
-
-	templateData.Footer = template.HTML(footerOutput)
-
-	navigationOutputs := make(map[string]template.HTML)
-	for name, content := range page.Navigations {
-		output, err := r.RenderOne(fmt.Sprintf("%s-navigation-%s", page.TemplateName, name), content, templateData)
-		if err != nil {
-			return "", err
-		}
-		navigationOutputs[name] = template.HTML(output)
-	}
-
-	templateData.Navigation = navigationOutputs
 
 	contentOutputs := make(map[string]template.HTML)
 	for slot, content := range page.Contents {
@@ -65,8 +36,53 @@ func (r *Renderer) RenderPage(page Page) (string, error) {
 
 		contentOutputs[slot] = template.HTML(output)
 	}
-
 	templateData.Content = contentOutputs
+
+	footerOutput, err := r.RenderOne(fmt.Sprintf("%s-footer", page.TemplateName), page.Footer, templateData)
+	if err != nil {
+		return "", err
+	}
+	templateData.Footer = template.HTML(footerOutput)
+
+	footerScriptOutput, err := r.RenderOne(fmt.Sprintf("%s-footscript", page.TemplateName), r.FootScript, templateData)
+	if err != nil {
+		return "", err
+	}
+	templateData.FootScript = template.HTML(footerScriptOutput)
+
+	headerOutput, err := r.RenderOne(fmt.Sprintf("%s-header", page.TemplateName), page.Header, templateData)
+	if err != nil {
+		return "", err
+	}
+	templateData.Header = template.HTML(headerOutput)
+
+	headerScriptOutput, err := r.RenderOne(fmt.Sprintf("%s-headscript", page.TemplateName), r.HeadScript, templateData)
+	if err != nil {
+		return "", err
+	}
+	templateData.HeadScript = template.HTML(headerScriptOutput)
+
+	metaOutput, err := r.RenderOne(fmt.Sprintf("%s-meta", page.TemplateName), r.Meta, templateData)
+	if err != nil {
+		return "", err
+	}
+	templateData.Meta = template.HTML(metaOutput)
+
+	navigationOutputs := make(map[string]template.HTML)
+	for name, content := range page.Navigations {
+		output, err := r.RenderOne(fmt.Sprintf("%s-navigation-%s", page.TemplateName, name), content, templateData)
+		if err != nil {
+			return "", err
+		}
+		navigationOutputs[name] = template.HTML(output)
+	}
+	templateData.Navigation = navigationOutputs
+
+	stylesheetOutput, err := r.RenderOne(fmt.Sprintf("%s-stylesheet", page.TemplateName), r.Stylesheet, templateData)
+	if err != nil {
+		return "", err
+	}
+	templateData.Stylesheet = template.HTML(stylesheetOutput)
 
 	return r.RenderOne(page.TemplateName, page.TemplateContent, templateData)
 }
