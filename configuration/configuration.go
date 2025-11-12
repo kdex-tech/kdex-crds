a45b6ce47a6344afbaeda371d73d6bdee931f931
Raymond Augé <rayauge@doublebite.com>
2025-11-12 14:22:22 -0500

    add role policies
    Signed-off-by: Raymond Augé <rayauge@doublebite.com>

--- configuration/configuration.go
+++ configuration/configuration.go
@@ -4,12 +4,14 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	gatewayv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
 type FocusControllerConfiguration struct {
-	Deployment appsv1.DeploymentSpec `json:"deployment" yaml:"deployment"`
-	Service    corev1.ServiceSpec    `json:"service" yaml:"service"`
+	Deployment      appsv1.DeploymentSpec `json:"deployment" yaml:"deployment"`
+	Service         corev1.ServiceSpec    `json:"service" yaml:"service"`
+	RolePolicyRules []rbacv1.PolicyRule   `json:"rolePolicyRules" yaml:"rolePolicyRules"`
 }
 
 type NexusConfiguration struct {
