cd9088b7b7d162ced82713bdbe17a8e242076db5
Raymond Augé <rayauge@doublebite.com>
2025-11-18 09:33:45 -0500

    fix mapping bug
    Signed-off-by: Raymond Augé <rayauge@doublebite.com>

--- npm/types.go
+++ npm/types.go
@@ -47,31 +47,35 @@ type PackageInfo struct {
 }
 
 type PackageJSON struct {
-	Author             string            `json:"author"`
+	Author             map[string]string `json:"author"`
 	Browser            string            `json:"browser"`
 	Bugs               interface{}       `json:"bugs"`
-	BundleDependencies []string          `json:"bundleDependencies"`
-	Dependencies       map[string]string `json:"dependencies"`
+	BundleDependencies []string          `json:"bundleDependencies,omitempty"`
+	Dependencies       map[string]string `json:"dependencies,omitempty"`
 	Description        string            `json:"description"`
-	DevDependencies    map[string]string `json:"devDependencies"`
+	DevDependencies    map[string]string `json:"devDependencies,omitempty"`
 	Dist               struct {
 		Integrity string `json:"integrity"`
 		Shasum    string `json:"shasum"`
 		Tarball   string `json:"tarball"`
 	} `json:"dist"`
-	Exports              map[string]string `json:"exports"`
-	Homepage             string            `json:"homepage"`
-	Keywords             []string          `json:"keywords"`
-	License              string            `json:"license"`
-	Main                 string            `json:"main"`
-	Name                 string            `json:"name"`
-	OptionalDependencies map[string]string `json:"optionalDependencies"`
-	PeerDependencies     map[string]string `json:"peerDependencies"`
-	Private              bool              `json:"private"`
-	Repository           interface{}       `json:"repository"`
-	Scripts              map[string]string `json:"scripts"`
-	Type                 string            `json:"type"`
-	Version              string            `json:"version"`
+	Exports              map[string]map[string]string `json:"exports,omitempty"`
+	Files                []string                     `json:"files,omitempty"`
+	GitHead              string                       `json:"gitHead,omitempty"`
+	Homepage             string                       `json:"homepage"`
+	Keywords             []string                     `json:"keywords"`
+	License              string                       `json:"license"`
+	Main                 string                       `json:"main,omitempty"`
+	Name                 string                       `json:"name"`
+	Module               string                       `json:"module,omitempty"`
+	OptionalDependencies map[string]string            `json:"optionalDependencies,omitempty"`
+	PeerDependencies     map[string]string            `json:"peerDependencies,omitempty"`
+	Private              bool                         `json:"private"`
+	ReadmeFilename       string                       `json:"readmeFilename"`
+	Scripts              map[string]string            `json:"scripts"`
+	Type                 string                       `json:"type,omitempty"`
+	Types                string                       `json:"types,omitempty"`
+	Version              string                       `json:"version"`
 }
 
 func (p *PackageJSON) HasESModule() error {
@@ -83,16 +87,20 @@ func (p *PackageJSON) HasESModule() error {
 		return nil
 	}
 
-	if p.Exports != nil {
-		browser, ok := p.Exports["browser"]
+	if p.Module != "" {
+		return nil
+	}
 
-		if ok && browser != "" {
+	if p.Exports != nil {
+		_, ok := p.Exports["browser"]
+
+		if ok {
 			return nil
 		}
 
-		imp, ok := p.Exports["import"]
+		_, ok = p.Exports["import"]
 
-		if ok && imp != "" {
+		if ok {
 			return nil
 		}
 	}
