e3e5838422b81076c91cb0f38a0e392a3ee9b9a7
Raymond Augé <rayauge@doublebite.com>
2025-12-22 11:08:24 -0500

    internal translation crd

--- api/v1alpha1/kdexinternaltranslation_types.go
+++ api/v1alpha1/kdexinternaltranslation_types.go
@@ -20,44 +20,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// KDexInternalTranslationSpec defines the desired state of KDexInternalTranslation
-type KDexInternalTranslationSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
-
-	// foo is an example field of KDexInternalTranslation. Edit kdexinternaltranslation_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
-}
-
-// KDexInternalTranslationStatus defines the observed state of KDexInternalTranslation.
-type KDexInternalTranslationStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	// conditions represent the current state of the KDexInternalTranslation resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
-	// +listType=map
-	// +listMapKey=type
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
-}
-
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 
@@ -69,13 +31,13 @@ type KDexInternalTranslation struct {
 	// +optional
 	metav1.ObjectMeta `json:"metadata,omitzero"`
 
-	// spec defines the desired state of KDexInternalTranslation
-	// +required
-	Spec KDexInternalTranslationSpec `json:"spec"`
+	// status defines the observed state of KDexApp
+	// +kubebuilder:validation:Optional
+	Status KDexObjectStatus `json:"status,omitempty,omitzero"`
 
-	// status defines the observed state of KDexInternalTranslation
-	// +optional
-	Status KDexInternalTranslationStatus `json:"status,omitzero"`
+	// spec defines the desired state of KDexTranslation
+	// +kubebuilder:validation:Required
+	Spec KDexTranslationSpec `json:"spec"`
 }
 
 // +kubebuilder:object:root=true
