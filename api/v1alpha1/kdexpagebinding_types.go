8de4680d6a2da8998badbf2b43c46bdfb2088be2
Raymond Augé <rayauge@doublebite.com>
2025-12-22 13:51:01 -0500

    label validation

--- api/v1alpha1/kdexpagebinding_types.go
+++ api/v1alpha1/kdexpagebinding_types.go
@@ -73,6 +73,8 @@ type KDexPageBindingSpec struct {
 	HostRef corev1.LocalObjectReference `json:"hostRef" protobuf:"bytes,2,req,name=hostRef"`
 
 	// label is the value used in menus and page titles before localization occurs (or when no translation exists for the current language).
+	// +kubebuilder:validation:MinLength=3
+	// +kubebuilder:validation:MaxLength=256
 	// +kubebuilder:validation:Required
 	Label string `json:"label" protobuf:"bytes,3,req,name=label"`
 
